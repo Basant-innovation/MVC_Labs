a695bbdfae12d9caf5dfe9241c39a2579ef8f3b1
Basant-innovation <59922781+Basant-innovation@users.noreply.github.com>
2020-05-04 02:23:00 +0200

    Add project files.

---
+++ MVC_Lab1/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Lab1.Controllers
+{
+
+    public class User
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+    }
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(User user)
+        {
+            if(user.Name != null && user.Email != null && user.Password != null)
+            {
+                ViewBag.Name = user.Name;
+                return View("Welcome");
+            }
+            return View();
+        }
+    }
+}

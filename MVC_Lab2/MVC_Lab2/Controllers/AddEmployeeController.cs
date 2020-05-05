adb1ba9f7fee802d0cd8e4138a069831bae06cb8
Basant-innovation <59922781+Basant-innovation@users.noreply.github.com>
2020-05-05 07:13:05 +0200

    Update Folders

---
+++ MVC_Lab2/MVC_Lab2/Controllers/AddEmployeeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Lab2.Controllers
+{
+    public class AddEmployeeController : Controller
+    {
+        // GET: AddEmployee
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
